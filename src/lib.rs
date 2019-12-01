f30700c5283cf290f9e61aae29225e527ca14d8c
Edmund Smith <edmund.pd.smith@gmail.com>
2019-12-01 06:12:29 +0000

    Add lib section to Cargo.toml

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+#![feature(custom_attribute)]
+extern crate continue_notation_macro;
+
+pub use continue_notation_macro::use_cps;
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        println!("It works!");
+    }
+
+    
+}
+
+    #[use_cps]
+    #[test]
+    fn cps_test() {
+        println!("Initial");
+        fn cps<F,R>(x:i32,mut f:F) -> R where F:FnMut(i32) -> R {
+            f(x)
+        }
+        let mut i = 0;
+        cont!(cps,(5,_) as x);
+        i+=1;
+        assert!(i==1);
+        println!("x={}",x);
+        {
+            cont!(cps, (x+2, _) as y);
+            i+=1;
+            assert!(i==2);
+            println!("y = {} = {} + 2 = x + 2", y, x);
+        }
+        i+=1;
+        assert!(i==3);
+    }
